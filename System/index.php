35753b4a72fd1d0677e6fbb1eebe27c67ae0e0f5
Sodiq sheriff <blazeemperor9@gmail.com>
2024-02-23 18:00:50 -0800

    work

--- System/index.php
+++ System/index.php
@@ -5,7 +5,6 @@ include 'constants/settings.php';
 include 'constants/check-login.php';
 ?>
 <head>
-
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
